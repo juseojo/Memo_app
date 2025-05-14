4eac2fc753b0e27c411c785cf7067a42b3059f71
juseojo <j86371224@gmail.com>
2025-05-14 17:51:07 +0900

    Feature : Add MemoViewModel
    - Implement MemoViewModel

--- Memo_app/ViewModel/MemoViewModel.swift
+++ Memo_app/ViewModel/MemoViewModel.swift
@@ -5,3 +5,21 @@
 //  Created by seongjun cho on 5/14/25.
 //
 
+import Foundation
+
+import Combine
+
+class MemoViewModel {
+	@Published var memo: [MemoEntity] = []
+	private var repository = MemoRepository()
+	private var cancellables = Set<AnyCancellable>()
+
+	init() {
+		memo = repository.load_memo()
+	}
+
+	func append_memo(content: MemoEntity) {
+		memo.append(content)
+		repository.save_memo(memo)
+	}
+}
