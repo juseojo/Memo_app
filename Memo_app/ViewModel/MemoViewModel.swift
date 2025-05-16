9c117e2bf75ff509ed5e2d4063f00e6e2af25353
juseojo <j86371224@gmail.com>
2025-05-16 16:28:53 +0900

    [Feature] Add append_memo, delete_memo to VM
    - Add append_memo, delete_memo to MemoViewModel
    - move cancellables to VC

--- Memo_app/ViewModel/MemoViewModel.swift
+++ Memo_app/ViewModel/MemoViewModel.swift
@@ -12,14 +12,20 @@ import Combine
 class MemoViewModel {
 	@Published var memo: [MemoEntity] = []
 	private var repository = MemoRepository()
-	private var cancellables = Set<AnyCancellable>()
 
 	init() {
 		memo = repository.load_memo()
 	}
 
-	func append_memo(content: MemoEntity) {
-		memo.append(content)
+	func append_memo(text: String) {
+		let new_memo = MemoEntity(id: (memo.last?.id ?? 0) + 1, text: text)
+
+		memo.append(new_memo)
+		repository.save_memo(memo)
+	}
+
+	func delete_memo(id: Int) {
+		memo.removeAll { $0.id == id }
 		repository.save_memo(memo)
 	}
 }
